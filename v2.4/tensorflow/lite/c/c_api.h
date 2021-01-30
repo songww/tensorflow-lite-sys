5136e8e968b311bbf9a1db452ccbb1acc5b43e99
Wonway Song <sww4718168@163.com>
2021-01-31 00:37:01 +0800

    Fix doc comments

--- v2.4/tensorflow/lite/c/c_api.h
+++ v2.4/tensorflow/lite/c/c_api.h
@@ -71,107 +71,107 @@ extern "C" {
 #endif  // __cplusplus
 
 // --------------------------------------------------------------------------
-// TfLiteVersion returns a string describing version information of the
-// TensorFlow Lite library. TensorFlow Lite uses semantic versioning.
+/// TfLiteVersion returns a string describing version information of the
+/// TensorFlow Lite library. TensorFlow Lite uses semantic versioning.
 TFL_CAPI_EXPORT extern const char* TfLiteVersion(void);
 
 // --------------------------------------------------------------------------
-// TfLiteModel wraps a loaded TensorFlow Lite model.
+/// TfLiteModel wraps a loaded TensorFlow Lite model.
 typedef struct TfLiteModel TfLiteModel;
 
-// Returns a model from the provided buffer, or null on failure.
+/// Returns a model from the provided buffer, or null on failure.
 TFL_CAPI_EXPORT extern TfLiteModel* TfLiteModelCreate(const void* model_data,
                                                       size_t model_size);
 
-// Returns a model from the provided file, or null on failure.
+/// Returns a model from the provided file, or null on failure.
 TFL_CAPI_EXPORT extern TfLiteModel* TfLiteModelCreateFromFile(
     const char* model_path);
 
-// Destroys the model instance.
+/// Destroys the model instance.
 TFL_CAPI_EXPORT extern void TfLiteModelDelete(TfLiteModel* model);
 
 // --------------------------------------------------------------------------
-// TfLiteInterpreterOptions allows customized interpreter configuration.
+/// TfLiteInterpreterOptions allows customized interpreter configuration.
 typedef struct TfLiteInterpreterOptions TfLiteInterpreterOptions;
 
-// Returns a new interpreter options instances.
+/// Returns a new interpreter options instances.
 TFL_CAPI_EXPORT extern TfLiteInterpreterOptions*
 TfLiteInterpreterOptionsCreate();
 
-// Destroys the interpreter options instance.
+/// Destroys the interpreter options instance.
 TFL_CAPI_EXPORT extern void TfLiteInterpreterOptionsDelete(
     TfLiteInterpreterOptions* options);
 
-// Sets the number of CPU threads to use for the interpreter.
+/// Sets the number of CPU threads to use for the interpreter.
 TFL_CAPI_EXPORT extern void TfLiteInterpreterOptionsSetNumThreads(
     TfLiteInterpreterOptions* options, int32_t num_threads);
 
-// Adds a delegate to be applied during `TfLiteInterpreter` creation.
-//
-// If delegate application fails, interpreter creation will also fail with an
-// associated error logged.
-//
-// NOTE: The caller retains ownership of the delegate and should ensure that it
-// remains valid for the duration of any created interpreter's lifetime.
+/// Adds a delegate to be applied during `TfLiteInterpreter` creation.
+///
+/// If delegate application fails, interpreter creation will also fail with an
+/// associated error logged.
+///
+/// NOTE: The caller retains ownership of the delegate and should ensure that it
+/// remains valid for the duration of any created interpreter's lifetime.
 TFL_CAPI_EXPORT extern void TfLiteInterpreterOptionsAddDelegate(
     TfLiteInterpreterOptions* options, TfLiteDelegate* delegate);
 
-// Sets a custom error reporter for interpreter execution.
-//
-// * `reporter` takes the provided `user_data` object, as well as a C-style
-//   format string and arg list (see also vprintf).
-// * `user_data` is optional. If provided, it is owned by the client and must
-//   remain valid for the duration of the interpreter lifetime.
+/// Sets a custom error reporter for interpreter execution.
+///
+/// * `reporter` takes the provided `user_data` object, as well as a C-style
+///   format string and arg list (see also vprintf).
+/// * `user_data` is optional. If provided, it is owned by the client and must
+///   remain valid for the duration of the interpreter lifetime.
 TFL_CAPI_EXPORT extern void TfLiteInterpreterOptionsSetErrorReporter(
     TfLiteInterpreterOptions* options,
     void (*reporter)(void* user_data, const char* format, va_list args),
     void* user_data);
 
 // --------------------------------------------------------------------------
-// TfLiteInterpreter provides inference from a provided model.
+/// TfLiteInterpreter provides inference from a provided model.
 typedef struct TfLiteInterpreter TfLiteInterpreter;
 
-// Returns a new interpreter using the provided model and options, or null on
-// failure.
-//
-// * `model` must be a valid model instance. The caller retains ownership of the
-//   object, and can destroy it immediately after creating the interpreter; the
-//   interpreter will maintain its own reference to the underlying model data.
-// * `optional_options` may be null. The caller retains ownership of the object,
-//   and can safely destroy it immediately after creating the interpreter.
-//
-// NOTE: The client *must* explicitly allocate tensors before attempting to
-// access input tensor data or invoke the interpreter.
+/// Returns a new interpreter using the provided model and options, or null on
+/// failure.
+///
+/// * `model` must be a valid model instance. The caller retains ownership of the
+///   object, and can destroy it immediately after creating the interpreter; the
+///   interpreter will maintain its own reference to the underlying model data.
+/// * `optional_options` may be null. The caller retains ownership of the object,
+///   and can safely destroy it immediately after creating the interpreter.
+///
+/// NOTE: The client *must* explicitly allocate tensors before attempting to
+/// access input tensor data or invoke the interpreter.
 TFL_CAPI_EXPORT extern TfLiteInterpreter* TfLiteInterpreterCreate(
     const TfLiteModel* model, const TfLiteInterpreterOptions* optional_options);
 
-// Destroys the interpreter.
+/// Destroys the interpreter.
 TFL_CAPI_EXPORT extern void TfLiteInterpreterDelete(
     TfLiteInterpreter* interpreter);
 
-// Returns the number of input tensors associated with the model.
+/// Returns the number of input tensors associated with the model.
 TFL_CAPI_EXPORT extern int32_t TfLiteInterpreterGetInputTensorCount(
     const TfLiteInterpreter* interpreter);
 
-// Returns the tensor associated with the input index.
-// REQUIRES: 0 <= input_index < TfLiteInterpreterGetInputTensorCount(tensor)
+/// Returns the tensor associated with the input index.
+/// REQUIRES: 0 <= input_index < TfLiteInterpreterGetInputTensorCount(tensor)
 TFL_CAPI_EXPORT extern TfLiteTensor* TfLiteInterpreterGetInputTensor(
     const TfLiteInterpreter* interpreter, int32_t input_index);
 
-// Resizes the specified input tensor.
-//
-// NOTE: After a resize, the client *must* explicitly allocate tensors before
-// attempting to access the resized tensor data or invoke the interpreter.
-// REQUIRES: 0 <= input_index < TfLiteInterpreterGetInputTensorCount(tensor)
+/// Resizes the specified input tensor.
+///
+/// NOTE: After a resize, the client *must* explicitly allocate tensors before
+/// attempting to access the resized tensor data or invoke the interpreter.
+/// REQUIRES: 0 <= input_index < TfLiteInterpreterGetInputTensorCount(tensor)
 TFL_CAPI_EXPORT extern TfLiteStatus TfLiteInterpreterResizeInputTensor(
     TfLiteInterpreter* interpreter, int32_t input_index, const int* input_dims,
     int32_t input_dims_size);
 
-// Updates allocations for all tensors, resizing dependent tensors using the
-// specified input tensor dimensionality.
-//
-// This is a relatively expensive operation, and need only be called after
-// creating the graph and/or resizing any inputs.
+/// Updates allocations for all tensors, resizing dependent tensors using the
+/// specified input tensor dimensionality.
+///
+/// This is a relatively expensive operation, and need only be called after
+/// creating the graph and/or resizing any inputs.
 TFL_CAPI_EXPORT extern TfLiteStatus TfLiteInterpreterAllocateTensors(
     TfLiteInterpreter* interpreter);
 
@@ -183,67 +183,67 @@ TFL_CAPI_EXPORT extern TfLiteStatus TfLiteInterpreterAllocateTensors(
 TFL_CAPI_EXPORT extern TfLiteStatus TfLiteInterpreterInvoke(
     TfLiteInterpreter* interpreter);
 
-// Returns the number of output tensors associated with the model.
+/// Returns the number of output tensors associated with the model.
 TFL_CAPI_EXPORT extern int32_t TfLiteInterpreterGetOutputTensorCount(
     const TfLiteInterpreter* interpreter);
 
-// Returns the tensor associated with the output index.
-// REQUIRES: 0 <= output_index < TfLiteInterpreterGetOutputTensorCount(tensor)
-//
-// NOTE: The shape and underlying data buffer for output tensors may be not
-// be available until after the output tensor has been both sized and allocated.
-// In general, best practice is to interact with the output tensor *after*
-// calling TfLiteInterpreterInvoke().
+/// Returns the tensor associated with the output index.
+/// REQUIRES: 0 <= output_index < TfLiteInterpreterGetOutputTensorCount(tensor)
+///
+/// NOTE: The shape and underlying data buffer for output tensors may be not
+/// be available until after the output tensor has been both sized and allocated.
+/// In general, best practice is to interact with the output tensor *after*
+/// calling TfLiteInterpreterInvoke().
 TFL_CAPI_EXPORT extern const TfLiteTensor* TfLiteInterpreterGetOutputTensor(
     const TfLiteInterpreter* interpreter, int32_t output_index);
 
 // --------------------------------------------------------------------------
-// TfLiteTensor wraps data associated with a graph tensor.
-//
-// Note that, while the TfLiteTensor struct is not currently opaque, and its
-// fields can be accessed directly, these methods are still convenient for
-// language bindings. In the future the tensor struct will likely be made opaque
-// in the public API.
-
-// Returns the type of a tensor element.
+/// TfLiteTensor wraps data associated with a graph tensor.
+///
+/// Note that, while the TfLiteTensor struct is not currently opaque, and its
+/// fields can be accessed directly, these methods are still convenient for
+/// language bindings. In the future the tensor struct will likely be made opaque
+/// in the public API.
+///
+/// Returns the type of a tensor element.
 TFL_CAPI_EXPORT extern TfLiteType TfLiteTensorType(const TfLiteTensor* tensor);
 
-// Returns the number of dimensions that the tensor has.
+/// Returns the number of dimensions that the tensor has.
 TFL_CAPI_EXPORT extern int32_t TfLiteTensorNumDims(const TfLiteTensor* tensor);
 
-// Returns the length of the tensor in the "dim_index" dimension.
-// REQUIRES: 0 <= dim_index < TFLiteTensorNumDims(tensor)
+/// Returns the length of the tensor in the "dim_index" dimension.
+/// REQUIRES: 0 <= dim_index < TFLiteTensorNumDims(tensor)
 TFL_CAPI_EXPORT extern int32_t TfLiteTensorDim(const TfLiteTensor* tensor,
                                                int32_t dim_index);
 
-// Returns the size of the underlying data in bytes.
+/// Returns the size of the underlying data in bytes.
 TFL_CAPI_EXPORT extern size_t TfLiteTensorByteSize(const TfLiteTensor* tensor);
 
-// Returns a pointer to the underlying data buffer.
-//
-// NOTE: The result may be null if tensors have not yet been allocated, e.g.,
-// if the Tensor has just been created or resized and `TfLiteAllocateTensors()`
-// has yet to be called, or if the output tensor is dynamically sized and the
-// interpreter hasn't been invoked.
+/// Returns a pointer to the underlying data buffer.
+///
+/// NOTE: The result may be null if tensors have not yet been allocated, e.g.,
+/// if the Tensor has just been created or resized and `TfLiteAllocateTensors()`
+/// has yet to be called, or if the output tensor is dynamically sized and the
+/// interpreter hasn't been invoked.
 TFL_CAPI_EXPORT extern void* TfLiteTensorData(const TfLiteTensor* tensor);
 
-// Returns the (null-terminated) name of the tensor.
+/// Returns the (null-terminated) name of the tensor.
 TFL_CAPI_EXPORT extern const char* TfLiteTensorName(const TfLiteTensor* tensor);
 
-// Returns the parameters for asymmetric quantization. The quantization
-// parameters are only valid when the tensor type is `kTfLiteUInt8` and the
-// `scale != 0`. Quantized values can be converted back to float using:
-//    real_value = scale * (quantized_value - zero_point);
+/// Returns the parameters for asymmetric quantization. The quantization
+/// parameters are only valid when the tensor type is `kTfLiteUInt8` and the
+/// `scale != 0`. Quantized values can be converted back to float using:
+///    real_value = scale * (quantized_value - zero_point);
 TFL_CAPI_EXPORT extern TfLiteQuantizationParams TfLiteTensorQuantizationParams(
     const TfLiteTensor* tensor);
 
-// Copies from the provided input buffer into the tensor's buffer.
-// REQUIRES: input_data_size == TfLiteTensorByteSize(tensor)
+/// Copies from the provided input buffer into the tensor's buffer.
+/// REQUIRES: input_data_size == TfLiteTensorByteSize(tensor)
 TFL_CAPI_EXPORT extern TfLiteStatus TfLiteTensorCopyFromBuffer(
     TfLiteTensor* tensor, const void* input_data, size_t input_data_size);
 
-// Copies to the provided output buffer from the tensor's buffer.
-// REQUIRES: output_data_size == TfLiteTensorByteSize(tensor)
+/// Copies to the provided output buffer from the tensor's buffer.
+/// REQUIRES: output_data_size == TfLiteTensorByteSize(tensor)
 TFL_CAPI_EXPORT extern TfLiteStatus TfLiteTensorCopyToBuffer(
     const TfLiteTensor* output_tensor, void* output_data,
     size_t output_data_size);
