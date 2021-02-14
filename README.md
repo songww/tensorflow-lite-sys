59a39adef08cf5e5fc1363ee2262241c327265b4
Wonway Song <sww4718168@163.com>
2021-02-14 22:23:43 +0800

    simple iOS framework linkage.

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # tensorflow-lite-sys
 
 This crate provides bindings to the raw low-level C API.
+
+## platforms
+[✅] With iOS framework(Manual Compilation or CocoaPods).
+[] With Android (Manual Compilation or AAR).
+[] Manual Compilation(libtensorflow-lite.a)
+
+## iOS Notice
+* static linked by default.
+* iOS dose not export experimental apis, enable it should with manual compilation framworks.
+* Set env `TFLITE_FRAMEWORK_PATH` for path that contains `TensorFlowLiteC.framework`, Likes:
+    >> ls Pods/TensorFlowLiteC/Frameworks/
+    TensorFlowLiteC.framework  TensorFlowLiteCCoreML.framework  TensorFlowLiteCMetal.framework.
+
+## Android Notice
+* dynamic linked [build android](https://www.tensorflow.org/lite/guide/build_android).
--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
 use bindgen;
 
@@ -69,4 +70,107 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .unwrap();
+
+    if cfg!(target_os = "ios") {
+        let coreml_required = cfg!(feature = "coreml");
+        let metal_required = cfg!(feature = "metal");
+
+        if let Ok(frameworks) = env::var("TFLITE_FRAMEWORK_PATH") {
+            let frameworks: &Path = frameworks.as_ref();
+            if !frameworks.join("TensorFlowLiteC.framework").exists() {
+                panic!(
+                    "`{}` dose not contains TensorFlowLiteC.framework",
+                    frameworks.display()
+                )
+            }
+            ios_framework_to_staticlib(
+                frameworks,
+                coreml_required,
+                metal_required,
+                &out_path.join("libtensorflow-lite.a"),
+            );
+        } else {
+            if let Some(pods_root) = find_pods_root() {
+                let frameworks = pods_root.join("TensorFlowLiteC").join("Frameworks");
+                if !frameworks.join("TensorFlowLiteC.framework").exists() {
+                    panic!("TensorFlowLiteC.framework dose not found, Please set env `TFLITE_FRAMEWORK_PATH` that contains the frameworks.")
+                }
+                ios_framework_to_staticlib(
+                    &frameworks,
+                    coreml_required,
+                    metal_required,
+                    &out_path.join("libtensorflow-lite.a"),
+                );
+            } else {
+                panic!("TensorFlowLiteC.framework dose not found, Please set env `TFLITE_FRAMEWORK_PATH` that contains the frameworks.")
+            }
+        }
+        println!("cargo:rustc-link-lib=static=tensorflow-lite");
+        println!("cargo:rustc-link-search=native={}", out_path.display());
+    }
+}
+
+fn find_pods_root() -> Option<PathBuf> {
+    if let Ok(pods_root) = env::var("PODS_ROOT") {
+        let p: &Path = pods_root.as_ref();
+        return Some(p.to_path_buf());
+    }
+    if let Ok(project_root) = env::var("PROJECT_DIR").or_else(|_| env::var("SRCROOT")) {
+        let p: &Path = project_root.as_ref();
+        let pods_root = p.join("Pods");
+        if pods_root.exists() {
+            return Some(pods_root);
+        }
+    }
+    if let Ok(project_root) = env::var("CARGO_MANIFEST_DIR") {
+        let p: &Path = project_root.as_ref();
+        let pods_root = p.join("Pods");
+        if pods_root.exists() {
+            return Some(pods_root);
+        }
+        let pods_root = p.join("..").join("Pods");
+        if pods_root.exists() {
+            return pods_root.canonicalize().ok();
+        }
+    }
+    return None;
+}
+
+fn ios_framework_to_staticlib(
+    frameworks: &Path,
+    coreml_required: bool,
+    metal_required: bool,
+    output: &Path,
+) {
+    let c = frameworks
+        .join("TensorFlowLiteC.framework")
+        .join("TensorFlowLiteC");
+    let mut objects = vec![c.as_os_str()];
+    let coreml = frameworks
+        .join("TensorFlowLiteCCoreML.framework")
+        .join("TensorFlowLiteCCoreML");
+    if coreml_required {
+        if coreml.exists() {
+            objects.push(coreml.as_os_str());
+        } else {
+            panic!("TensorFlowLiteCCoreML.framework dose not exists.");
+        }
+    }
+    let metal = frameworks
+        .join("TensorFlowLiteCMetal.framework")
+        .join("TensorFlowLiteCMetal");
+    if metal_required {
+        if metal.exists() {
+            objects.push(metal.as_os_str());
+        } else {
+            panic!("TensorFlowLiteCMetal.framework dose not exists.");
+        }
+    }
+    Command::new("libtool")
+        .arg("-static")
+        .args(&objects)
+        .arg("-o")
+        .arg(output.as_os_str())
+        .output()
+        .expect("Failed to create libtensorflow-lite.a");
 }
